2a8789ee550c09b89c81e3ae64230f2ace81a6a3
Vatsal Patel <pvatsal23@gmail.com>
2018-10-03 22:41:39 -0400

    Added phone call broadcast receiver.

---
+++ app/src/main/java/com/dnd/vatsaldipen/dnd/CallReceiver.java
@@ -0,0 +1,38 @@
+package com.dnd.vatsaldipen.dnd;
+
+import android.content.Context;
+import java.util.Date;
+
+public class CallReceiver extends PhonecallReceiver {
+
+    @Override
+    protected void onIncomingCallStarted(Context ctx, String number, Date start) {
+        super.onIncomingCallStarted(ctx, number, start);
+        // TODO send message to the number.
+    }
+
+    @Override
+    protected void onOutgoingCallStarted(Context ctx, String number, Date start) {
+        super.onOutgoingCallStarted(ctx, number, start);
+    }
+
+    @Override
+    protected void onIncomingCallEnded(Context ctx, String number, Date start, Date end) {
+        super.onIncomingCallEnded(ctx, number, start, end);
+    }
+
+    @Override
+    protected void onOutgoingCallEnded(Context ctx, String number, Date start, Date end) {
+        super.onOutgoingCallEnded(ctx, number, start, end);
+    }
+
+    @Override
+    protected void onMissedCall(Context ctx, String number, Date start) {
+        super.onMissedCall(ctx, number, start);
+    }
+
+    @Override
+    public void onCallStateChanged(Context context, int state, String number) {
+        super.onCallStateChanged(context, state, number);
+    }
+}
